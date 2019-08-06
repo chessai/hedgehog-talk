a28eb5c13cd08bc15830d32aa8d5b5c5fe949e59
chessai <chessai1996@gmail.com>
2019-08-06 19:26:56 -0400

    add hedgehog-classes example

---
+++ src/Class.hs
@@ -0,0 +1,59 @@
+module Class where
+
+import Hedgehog.Classes
+
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+import Data.Bifunctor
+import Data.Map (Map)
+import Data.Set (Set)
+import qualified Data.List as List
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+genEither :: Gen a -> Gen b -> Gen (Either a b)
+genEither genA genB = Gen.frequency [ (1, Left <$> genA), (1, Right <$> genB) ]
+
+genSet :: Ord a => Gen a -> Gen (Set a)
+genSet genA = Set.fromList <$> Gen.list (Range.linear 0 100) genA
+
+genMap :: Ord k => Gen k -> Gen a -> Gen (Map k a)
+genMap genK genA = do
+  let sz = Range.linear 0 100
+  ks <- Gen.list sz genK
+  as <- Gen.list sz genA
+  pure $ Map.fromList (List.zip ks as)
+
+genString :: Gen String
+genString = Gen.string (Range.linear 2 6) Gen.alpha
+
+genInt :: Gen Int
+genInt = Gen.int Range.constantBounded
+
+genSet0 :: Gen (Set Int)
+genSet0 = genSet genInt
+
+genMap0 :: Gen (Map String Int)
+genMap0 = genMap genString genInt
+
+genMap1 :: Gen a -> Gen (Map String a)
+genMap1 = genMap genString
+
+genEither0 :: Gen (Either String Int)
+genEither0 = genEither genString genInt
+
+genEither1 :: Gen a -> Gen (Either String a)
+genEither1 = genEither genString
+
+tests :: IO Bool
+tests = lawsCheckMany
+  [ ("Set Int", [eqLaws genSet0, monoidLaws genSet0])
+  , ("Map String Int", [eqLaws genMap0, monoidLaws genMap0])
+  , ("Map String", [functorLaws genMap1])
+  , ("Either String Int", [eqLaws genEither0])
+  , ("Either String", [functorLaws genEither1, applicativeLaws genEither1, traversableLaws genEither1])
+  , ("Either", [bifunctorLaws genEither])
+  ]
+
